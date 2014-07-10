535cb738d3a59bace71fe32868148d3ca21858c4
Attila Maczak <attila@maczak.hu>
2014-07-10 12:34:14 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,26 +1,4 @@
 #!/bin/bash
-
-# example nas:/mnt/biggins/music
-nfs_mount=${NFS_MOUNT}
 mountpoint="/var/lib/mpd/music"
-
-if [ ! $nfs_mount ]; then
-  echo ""
-  echo "***************************************"
-  echo "must set NFS_MOUNT environment variable"
-  echo "***************************************"
-  echo ""
-
-  echo "EXAMPLE:"
-  echo "docker run -privileged -e NFS_MOUNT=nas:/mnt/biggins/music -p 6600:6600 -p 8000:8000 -t narf/mpd"
-  echo ""
-  exit 1
-fi
-
-rpcbind &&
-rpc.statd &&
-
-#mount -t nfs -o timeo=600 -o tcp -o hard -o bg $nfs_mount $mountpoint -vvvv &&
-mount -t nfs $nfs_mount $mountpoint -vvv &&
 
 mpd --no-daemon --stdout -v /etc/mpd.conf
